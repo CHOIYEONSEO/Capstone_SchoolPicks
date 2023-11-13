bb2920e643ad99e5d6b4d65c849b0256e48dadb2
Seo <vmrmrtms123@naver.com>
2023-11-13 23:21:15 +0900

    all front part

---
+++ src/main/resources/templates/js/find_mate.js
@@ -0,0 +1,6 @@
+var groupContainer8 = document.getElementById("groupContainer8");
+if (groupContainer8) {
+    groupContainer8.addEventListener("click", function (e) {
+        window.location.href = "./find-mate-ver1.html";
+    });
+}
---
+++ src/main/resources/templates/js/reviewer.js
@@ -0,0 +1,6 @@
+var topSearchBar1 = document.getElementById("topSearchBar1");
+if (topSearchBar1) {
+    topSearchBar1.addEventListener("click", function (e) {
+        window.location.href = "./reviewver21.html";
+    });
+}
