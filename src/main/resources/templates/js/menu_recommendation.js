b16792f71c849c61e92b6007fb2108a46da081d7
YEONSEO <yeonseochoi0717@gmail.com>
2023-11-15 13:00:14 +0900

    modify menu_recommendation and add second function(find mate & history)

--- src/main/resources/templates/js/menu_recommendation.js
+++ src/main/resources/templates/js/menu_recommendation.js
@@ -2,39 +2,8 @@ const restaurantTag = document.getElementById('tag_restaurant');
 const cafeTag = document.getElementById('tag_cafe');
 const pubTag = document.getElementById('tag_pub');
 
-let restaurant_count = 1;
-let cafe_count = 1;
-let pub_count = 1;
+const detailBtn = document.getElementById('goto_detail');
 
-/* 음식점 태그 클릭시 색 변환 */
-restaurantTag.addEventListener("click", (e) => {
-    if (restaurant_count % 2 == 1) {
-        e.target.style.backgroundColor = "rgba(252,180,221,0.8)";
-    } else {
-        e.target.style.backgroundColor = "#DFDFDF";
-    }
-    restaurant_count += 1;
-});
-
-/* 카페 태그 클릭시 색 변환 */
-cafeTag.addEventListener("click", (e) => {
-    if (cafe_count % 2 == 1) {
-        e.target.style.backgroundColor = "rgba(254,214,98,0.8)";
-    } else {
-        e.target.style.backgroundColor = "#DFDFDF";
-    }
-    cafe_count += 1;
-});
-
-/* 술집 태그 클릭시 색 변환 */
-pubTag.addEventListener("click", (e) => {
-    if (pub_count % 2 == 1) {
-        e.target.style.backgroundColor = "rgba(164,231,231,0.8)";
-    } else {
-        e.target.style.backgroundColor = "#DFDFDF";
-    }
-    pub_count += 1;
-});
 
 /* 자판기 클릭시 추천 음식점 변경 */
 /* 자과캠 */
@@ -51,4 +20,8 @@ vendingMachine.addEventListener("click", (e) => {
     var recommendation = '나누미떡볶이';
 
     document.getElementById('recommendation_HSSC').innerHTML = recommendation;
+})
+
+detailBtn.addEventListener("click", (e) => {
+    alert("음식점 세부 정보");
 })
