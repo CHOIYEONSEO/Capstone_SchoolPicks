430c67dad1697f7a18d76b4d5417a0211e53ff48
compasstar <woohong24@naver.com>
2023-11-27 00:06:28 +0900

    feat: ver2, ver3 인스타적용

--- src/main/java/com/example/demo/controller/FindMateController.java
+++ src/main/java/com/example/demo/controller/FindMateController.java
@@ -263,6 +263,7 @@ public class FindMateController {
             time = "오전 " + hour + ":" + minute;
         }
 
+        findMateRoomPageForm.setShopName(findMateRoomPageDto.getShopName());
         findMateRoomPageForm.setYear(year);
         findMateRoomPageForm.setMonth(month);
         findMateRoomPageForm.setDate(date);
@@ -276,6 +277,88 @@ public class FindMateController {
         model.addAttribute("roomId", roomId);
 
         return "ver1-instagram-story";
+    }
+
+    @GetMapping("/mate/instagram/ver2/{roomId}")
+    public String shareInstagramVer2(@PathVariable String roomId, @ModelAttribute("findMateRoomPage") FindMateRoomPageForm findMateRoomPageForm, Model model) {
+
+        FindMateRoomPageDto findMateRoomPageDto;
+        if (isPrivate) {
+            findMateRoomPageDto = findMateRoomService.showFindMateRoomWithBlindMode(roomId);
+        } else {
+            findMateRoomPageDto = findMateRoomService.showFindMateRoom(roomId);
+        }
+
+        String localDateTime = findMateRoomPageDto.getLocalDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-E-HH-mm-ss"));
+        String[] localDateTimeSplit = localDateTime.split("-");
+        String year = localDateTimeSplit[0];
+        String month = localDateTimeSplit[1];
+        String date = localDateTimeSplit[2];
+        String day = "(" + localDateTimeSplit[3] + ")";
+        int hour = Integer.parseInt(localDateTimeSplit[4]);
+        String minute = localDateTimeSplit[5];
+        String time;
+        if (hour >= 12) {
+            time = "오후 " + (hour - 12) + ":" + minute;
+        } else {
+            time = "오전 " + hour + ":" + minute;
+        }
+
+        findMateRoomPageForm.setShopName(findMateRoomPageDto.getShopName());
+        findMateRoomPageForm.setYear(year);
+        findMateRoomPageForm.setMonth(month);
+        findMateRoomPageForm.setDate(date);
+        findMateRoomPageForm.setDay(day);
+        findMateRoomPageForm.setTime(time);
+        findMateRoomPageForm.setHeadCount(findMateRoomPageDto.getHeadCount());
+        findMateRoomPageForm.setRoomWriter(findMateRoomPageDto.getRoomWriter());
+        findMateRoomPageForm.setRoomMessage(findMateRoomPageDto.getRoomMessage());
+        findMateRoomPageForm.setUsers(findMateRoomPageDto.getUsers());
+
+        model.addAttribute("roomId", roomId);
+
+        return "ver2-instagram-story";
+    }
+
+    @GetMapping("/mate/instagram/ver3/{roomId}")
+    public String shareInstagramVer3(@PathVariable String roomId, @ModelAttribute("findMateRoomPage") FindMateRoomPageForm findMateRoomPageForm, Model model) {
+
+        FindMateRoomPageDto findMateRoomPageDto;
+        if (isPrivate) {
+            findMateRoomPageDto = findMateRoomService.showFindMateRoomWithBlindMode(roomId);
+        } else {
+            findMateRoomPageDto = findMateRoomService.showFindMateRoom(roomId);
+        }
+
+        String localDateTime = findMateRoomPageDto.getLocalDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-E-HH-mm-ss"));
+        String[] localDateTimeSplit = localDateTime.split("-");
+        String year = localDateTimeSplit[0];
+        String month = localDateTimeSplit[1];
+        String date = localDateTimeSplit[2];
+        String day = "(" + localDateTimeSplit[3] + ")";
+        int hour = Integer.parseInt(localDateTimeSplit[4]);
+        String minute = localDateTimeSplit[5];
+        String time;
+        if (hour >= 12) {
+            time = "오후 " + (hour - 12) + ":" + minute;
+        } else {
+            time = "오전 " + hour + ":" + minute;
+        }
+
+        findMateRoomPageForm.setShopName(findMateRoomPageDto.getShopName());
+        findMateRoomPageForm.setYear(year);
+        findMateRoomPageForm.setMonth(month);
+        findMateRoomPageForm.setDate(date);
+        findMateRoomPageForm.setDay(day);
+        findMateRoomPageForm.setTime(time);
+        findMateRoomPageForm.setHeadCount(findMateRoomPageDto.getHeadCount());
+        findMateRoomPageForm.setRoomWriter(findMateRoomPageDto.getRoomWriter());
+        findMateRoomPageForm.setRoomMessage(findMateRoomPageDto.getRoomMessage());
+        findMateRoomPageForm.setUsers(findMateRoomPageDto.getUsers());
+
+        model.addAttribute("roomId", roomId);
+
+        return "ver3-instagram-story";
     }
 
 
