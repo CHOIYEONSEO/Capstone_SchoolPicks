11a4401cedaa6b1addc9a8467beec6adf595f993
YEONSEO <yeonseochoi0717@gmail.com>
2023-12-01 02:49:28 +0900

    take x position and y position text from detail.html to detail.js and hide text

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -1,12 +1,15 @@
+var mapXPosition = document.getElementById("map_x_position").innerText;
+var mapYPosition = document.getElementById("map_y_position").innerText;
+
 var mapOptions = {
-    center: new naver.maps.LatLng(37.293927, 126.974376),
+    center: new naver.maps.LatLng(mapXPosition, mapYPosition),
     zoom: 15
 };
 
 var map = new naver.maps.Map('map', mapOptions);
 
 var marker = new naver.maps.Marker({
-    position: new naver.maps.LatLng(37.293927, 126.974376), /* 2번째줄 경도 위도와 동일한 값으로*/
+    position: new naver.maps.LatLng(mapXPosition, mapYPosition), /* 5번째줄 경도 위도와 동일한 값으로*/
     map: map,
     icon: {
         url: '/images/logo.png',
