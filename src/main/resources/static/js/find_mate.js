9cf650e8da32e82d22babdd06eb9f9f169f9e411
yeonseo <yeonseochoi0717@gmail.com>
2023-12-03 13:12:55 +0900

    default setting for appointment time and close time in find mate page

--- src/main/resources/static/js/find_mate.js
+++ src/main/resources/static/js/find_mate.js
@@ -1,3 +1,11 @@
+let date = new Date();
+date.setHours(date.getHours() + 1);
+document.getElementById('tempt3').value= date.toISOString().slice(0, 16);
+
+let closeTime = new Date();
+closeTime.setMinutes(closeTime.getMinutes() + 50);
+document.getElementById('tempt8').value= closeTime.toISOString().slice(0, 16);
+
 
 function check(){
   if (confirm("모든 항목을 정확히 입력하셨습니까?") == true){    //확인
