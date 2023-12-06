291927100469b9f6a0a7fb702d206b89baa0514a
YEONSEO <yeonseochoi0717@gmail.com>
2023-12-06 14:09:21 +0900

    chagne default time setting to Korean time

--- src/main/resources/static/js/form_default.js
+++ src/main/resources/static/js/form_default.js
@@ -1,7 +1,10 @@
-let date = new Date();
+//한국시간으로 맞추기
+const offset = 1000 * 60 * 60 * 9;
+
+let date = new Date((new Date()).getTime() + offset);
 date.setHours(date.getHours() + 1);
 document.getElementById('tempt3').value= date.toISOString().slice(0, 16);
 
-let closeTime = new Date();
+let closeTime = new Date((new Date()).getTime() + offset);
 closeTime.setMinutes(closeTime.getMinutes() + 50);
 document.getElementById('tempt8').value= closeTime.toISOString().slice(0, 16);
